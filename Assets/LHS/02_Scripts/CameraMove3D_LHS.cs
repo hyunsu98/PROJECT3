2aa71141c87c22b8b49b6a61966bbcaa5b831799
hyunsu98 <108320515+hyunsu98@users.noreply.github.com>
2022-09-12 20:46:26 +0900

    카메라 3D추가

--- Assets/LHS/02_Scripts/CameraMove3D_LHS.cs
+++ Assets/LHS/02_Scripts/CameraMove3D_LHS.cs
@@ -2,71 +2,96 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CameraMove3D_LHS : MonoBehaviour
 {
-    //public Transform target;
+    public List<Transform> targets;
 
-    // 따라다닐 타겟1
-    public Transform target1;
-    public Transform target2;
+    // 중심점에서 카메라 조절하기위해
+    public Vector3 offset;
+    public float smoothTime = 0.5f;
 
-    // 타겟들의 중심점
-    Transform centerTarget;
+    // 줌아웃 크기
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    // 확대/축소 제한기
+    public float zoomLimiter = 50f;
 
-    // 이동 속도
-    public float speed;
+    // 이동속도
+    private Vector3 velocity;
+    private Camera cam;
 
-    //카메라 제한 영역
-    //중심점
-    public Vector2 center;
-    //크기
-    public Vector2 size;
+    Vector3 newPosition;
 
-    float height;
-    float width;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        // 시작 할 때 위치 = 타겟 위치로
-        //transform.position = target.position;
-        //x 회전 각도
-        //transform.eulerAngles = new Vector3(6, 0, 0);
-
-        //카메라의 월드공간에서 가로/ 세로의 크기를 구해야함
-        //세로 절반 크기
-        //height = Camera.main.orthographicSize;
-        //월드 가로 = 월드 세로 * 스크린 가로/ 스크린 세로
-        //width = height * Screen.width / Screen.height;
+        cam = GetComponent<Camera>();
     }
 
-    //변수값을 시각적으로 보기 위해 함수 생성
-    private void OnDrawGizmos()
+    private void LateUpdate()
     {
-        //Gizmos.color = Color.red;
-        //Gizmos.DrawWireCube(center, size);
+        // 만약 타겟이 0이라면 아무것도 하지 않는다
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        Zoom();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Zoom()
     {
-        centerTarget.position = (target1.position + target2.position) * 0.5f;
-
-        //Lerp를 이용한 카메라 이동(target 따라다니게)
-        transform.position = Vector3.Lerp(transform.position, centerTarget.position, Time.deltaTime * speed);
-        //z축 
-        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
-
-        // Mathf.Clamp(value,min,max) 
-        // value값이 min과 max의 사이면 value를 반환하고
-        // value값이 min보다 작으면 min을 max보다 크면 max를 반환
-        //float lx = size.x * 0.5f - width;
-        //float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
-
-        //float ly = size.y * 0.5f - height;
-        //float clampY = Mathf.Clamp(transform.position.y, -ly + center.y, lx + center.y);
-
-        //transform.position = new Vector3(clampX, clampY, -10f); 
-
+        // 최대거리 설정
+        //Debug.Log(GetGreatestDistance());
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance() / zoomLimiter);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime);
     }
+
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            // 캡슐화
+            bounds.Encapsulate(targets[i].position);
+        }
+        // 상자의 너비
+        return bounds.size.x;
+    }
+
+    // 카메라 움직임
+    void Move()
+    {
+        // 카메라 위치 = 모든 목표의 중심으로 설정
+        Vector3 centerPoint = GetCenterPoint();
+
+        // 중심점에서 + 내가 위치 조절
+        Vector3 newPosition = centerPoint + offset;
+
+        // 내 위치 = 중심점
+        //transform.position = newPosition;
+        // 부드럽게 움직이기 위해 
+        transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+    }
+
+    // 중심점 찾기
+    Vector3 GetCenterPoint()
+    {
+        // 만약 타겟이 한명이라면
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        // 여러개의 대상의 중심점을 구하는 방법
+        // BOUNDS 여러개체를 캡슐화하는 데 사용할 수 있는 클래스
+
+        // 첫번째 대상 중심에서 시작, 크기는 0으로
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+
 }
