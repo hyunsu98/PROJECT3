9847246824eb283a7dbb01e61901569db3e1f528
mynameisjinhohong <110793489+mynameisjinhohong@users.noreply.github.com>
2022-09-18 14:00:39 +0900

    스킬 이펙트 날라가는거 수정

--- Assets/HJH/Scripts/Weapon_HJH.cs
+++ Assets/HJH/Scripts/Weapon_HJH.cs
@@ -7,6 +7,7 @@ public class Weapon_HJH : MonoBehaviour
     public bool Attack = false;
     public int Damage = 10;
     public float skillSpeed = 2f;
+    bool check = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,20 +17,39 @@ public class Weapon_HJH : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(this.name.Contains("Player1SkillEffect"))
+        if(this.name.Contains("Player1SkillEffect") && check == true)
         {
-            if(GameObject.Find("Player1").transform.eulerAngles.y > 0)
+            if(GameObject.Find("Player1").transform.rotation.y > 0)
             {
-                transform.position += Vector3.right * Time.deltaTime * skillSpeed;
-                gameObject.GetComponent<Renderer>().sortingOrder = 50;
+                StartCoroutine(Right());
+                check = false;
             }
             else
             {
-                Debug.Log("??");
-                transform.position += Vector3.left * Time.deltaTime * skillSpeed;
-                gameObject.GetComponent<Renderer>().sortingOrder = 50;
+                StartCoroutine(Left());
+                check = false;
             }
         }
+    }
+    IEnumerator Right()
+    {
+        while (true)
+        {
+            transform.position += Vector3.right * Time.deltaTime * skillSpeed;
+            gameObject.GetComponent<Renderer>().sortingOrder = 50;
+            yield return null;
+        }
+        
+    }
+    IEnumerator Left()
+    {
+        while (true)
+        {
+            transform.position += Vector3.left * Time.deltaTime * skillSpeed;
+            gameObject.GetComponent<Renderer>().sortingOrder = 50;
+            yield return null;
+        }
+
     }
     private void OnTriggerEnter(Collider other)
     {
